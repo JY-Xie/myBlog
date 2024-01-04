68954cd6a2653e139bd0a2c52b5bb96fdb6d0f34
xiejiye <xiejy2001@163.com>
2024-01-04 16:02:35 +0800

    adjust delete

--- mysite/auth/auth_manage.py
+++ mysite/auth/auth_manage.py
@@ -4,7 +4,7 @@ from flask import Blueprint, flash, g, redirect, render_template, request, sessi
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.security import check_password_hash, generate_password_hash
-from ..model.tables import Photo
+from ..model.tables import Photo, Article
 
 bp = Blueprint('auth_manage', __name__, url_prefix='/auth_manage')
 engine = create_engine("sqlite:///mysite/model/database.db", echo=True)
@@ -38,7 +38,7 @@ db_session = scoped_session(sessionmaker(autocommit=False,
 #
 #     return render_template('blog/create.html')
 
-@bp.route('/')
+@bp.route('/photo_manage')
 @login_required
 def manage_photo():
     page = int(request.args.get('page', 1))
@@ -59,8 +59,40 @@ def manage_photo():
 
 
 @login_required
-def delete(id):
+@bp.route('/<int:id>/photo_delete', methods=('POST',))
+def delete_photo(id):
     to_del = db_session.query(Photo).get(id)
     db_session.delete(to_del)
     db_session.commit()
     return redirect(url_for('auth_manage.manage_photo'))
+
+
+
+@bp.route('/article_manage')
+@login_required
+def manage_article():
+    page = int(request.args.get('page', 1))
+    per_page = 10
+    db_list = db_session.query(Article.id, Article.article_head, Article.article_abstract).all()
+    total_length = len(db_list)
+    total_pages = len(db_list) // per_page + 1
+    start_index = (page - 1) * per_page
+    end_index = start_index + per_page
+    current_page_content = db_list[start_index:end_index]
+    print(db_list)
+    return render_template(
+            './auth/auth_manage_article.html',
+            abstract_list=current_page_content,
+            page=page,
+            total_pages=total_pages,
+            total_length=total_length
+        )
+
+
+@login_required
+@bp.route('/<int:id>/article_delete', methods=('POST',))
+def delete_article(id):
+    to_del = db_session.query(Article).get(id)
+    db_session.delete(to_del)
+    db_session.commit()
+    return redirect(url_for('auth_manage.manage_article'))
